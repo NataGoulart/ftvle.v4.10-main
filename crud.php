9a17db2f67d20dd1733f8b89d93dd4b61bb71fcd
NataGoulart <nata_santos@estudante.sesisenai.org.br>
2023-11-09 09:14:19 -0300

    first commit

---
+++ crud.php
@@ -0,0 +1,25 @@
+
+        <?php
+        $id=$_POST["id"];
+        $nome=$_POST["produto"];
+        $email=$_POST["codigo"];
+        include("conecta.php");
+        
+        
+        if (isset($_POST["excluir"])) {
+            $comando = $pdo->prepare("DELETE FROM codigos WHERE id='$id'");
+            $resultado = $comando->execute();
+            header("Location: salvar_imagem.php");
+        }
+        
+        if (isset($_POST["alterar"])) {
+            $comando = $pdo->prepare("UPDATE codigos SET produto='$produto',codigo='$codigo' WHERE id='$id'");
+            $resultado = $comando->execute();
+            header("Location: salvar_imagem.php");
+        }
+        
+        if (isset($_POST["listar"])) {
+            header("Location: salvar_imagem.php?email=$email");
+        }
+        
+        ?>
